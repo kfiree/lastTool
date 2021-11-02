c9db57f7dde7c4e0813aa55ba0d8b1c20ec18b8b
ilanteit <45786520+ilanteit@users.noreply.github.com>
2021-11-02 17:17:54 +0200

    Update Ex1.c
    second try

--- Ex1.c
+++ Ex1.c
@@ -5,43 +5,71 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <paths.h>
+#include <sys/types.h>
+#include <bits/wordsize.h>
 
 
 void showtime(long );
 void show_info(struct utmp *);
-void removeChar(char *, char );
+
 void show_more();
 
-int main()
-{
-    struct utmp utbuf;
-    int utmpfd;
-    if((utmpfd=open(UTMP_FILE, O_RDONLY))== -1)
-       { 
-        perror(UTMP_FILE);
-        exit(1);
-       }
-    while (read(utmpfd,&utbuf, sizeof(utbuf))==sizeof(utbuf))
-        show_info(&utbuf);
+int main(int argc, char *argv[]){
 
-    close(utmpfd);
+    int log_num=0;
+    if( argc == 2 ) {
+        log_num=atoi(argv[1]);
+    }
+
+
+
+
+    struct utmp utbuf;
+    FILE * fd;
+//    if((fd=open(WTMP_FILE, O_RDONLY))== -1) {
+//        perror(WTMP_FILE);
+//        exit(1);
+//    }
+    fd = fopen("/var/log/wtmp", "r ");
+    if(!fd){ perror("File not open"); exit(1);}
+
+    fseek(fd,sizeof(utbuf),SEEK_END-log_num*sizeof(utbuf));
+
+    int i = 0;
+    while(log_num>0){
+        fread(&utbuf,sizeof(struct utmp), 1,fd);
+        if(strcmp(&utbuf.ut_user,"runlevel")!=0 && strcmp(&utbuf.ut_user,"shutdown")!=0) {
+            show_info(&utbuf);
+            log_num--;
+        }
+    }
+
+    close(fd);
     return 0;
-    
+
 }
 
 void show_info(struct utmp *utbufp){
-    if(utbufp-> ut_type != USER_PROCESS)
-        return ;
-    printf("%-8.8s", utbufp->ut_name);
+
+    // print username
+    printf("%-8.8s", utbufp->ut_user);
     printf(" ");
-    printf("%-8.8s", utbufp->ut_line);
+
+    //print line
+    if(strcmp(utbufp->ut_user, "reboot") == 0){
+        printf("system boot  ");
+    }else{
+        printf("%-8.8s    ", utbufp->ut_line);
+        printf(" ");
+    }
+
+    // print host (version)
+    printf("%s", utbufp->ut_host);
     printf(" ");
     showtime(utbufp->ut_time);
-#ifdef SHOWHOST
-    if(utbufp->ut_host[0]!= '\0')
-        printf(" (%s)",utbufp->ut_host);
-#endif
-    show_more();
+
+
     printf("\n");
 
 }
@@ -50,44 +78,27 @@ void showtime(long timeval){
     char *cp;
     cp= ctime(&timeval);
     int size = strlen(cp); //Total size of string
-    
-    cp[size-5] = '\0';
-    
 
-    printf("%s", cp);
+    cp[size-5] = '\0';
+
+
+    printf("%12.16s", cp);
+
     //printf("%12.12s", cp+4);
 }
-void show_more(){
-    struct utmp *line;
-    utmpname("/var/log/wtmp");
-    printf(utmpname);
-        setutent();
-        while( (line = getutent()) != NULL) {
-                if (line->ut_type == USER_PROCESS ) {
-                        printf("%s,%s,%s,%s,%d", line->ut_user,line->ut_user,line->ut_id,line->ut_line,line->ut_type);
-                        printf("\n");
-                }
-        }
-        endutent();
-        return;
+//void show_more(){
+//    struct lastlog *line;
+//    utmpname("/var/log/lastlog");
+//    printf(utmpname);
+//        setutent();
+//        while( (line = getutent()) != NULL) {
+//
+//                        printf("%s,%s,%d", line->ll_host,line->ll_line,line->ll_time);
+//                        printf("\n");
+//
+//        }
+//        endutent();
+//        return;
+//
+//}
 
-}
-// void removeChar(char *str, char garbage) {
-
-//     char *src, *dst;
-//     for (src = dst = str; *src != '\0'; src++) {
-//         *dst = *src;
-//         if (*dst != garbage) dst++;
-//     }
-//     *dst = '\0';
-// }
-void removeChar(char* s, char c)
-{
- 
-    int j, n = strlen(s);
-    for (int i = j = 0; i < n; i++)
-        if (s[i] != c)
-            s[j++] = s[i];
- 
-    s[j] = '\0';
-}
